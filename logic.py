5de3e97f55a045df8b2ef6938f296040487231e5
Justin_Kinsey <justinkinsey95@gmail.com>
2025-11-25 10:40:57 -0600

    first push. GUI design finished.

---
+++ logic.py
@@ -0,0 +1,34 @@
+from gui import *
+from PyQt6.QtWidgets import *
+from voter import Voter
+import csv
+import os
+
+class Logic(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        #TODO: wipe screen at the start >
+
+        self.buttonGroup_candidates
+
+
+
+
+    def hide_fields(self):
+        pass
+
+    def check_voter_id(self):
+        pass
+
+    def get_candidate(self):
+        pass
+
+    def check_opt_in(self):
+        pass
+
+    def submit(self):
+        pass
+
+
+
---
+++ voter.py
@@ -0,0 +1,5 @@
+class Voter:
+    def __init__(self, id, party, opt_in):
+        self.__voter_id = id
+        self.__party = party
+        self.__opt_in = opt_in
