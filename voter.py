0143ef17c7d712ac4c22f100639cde56b26cf079
Justin_Kinsey <justinkinsey95@gmail.com>
2025-11-28 00:36:56 -0600

    app is functional and ready for rveiew

--- voter.py
+++ voter.py
@@ -3,3 +3,24 @@ class Voter:
         self.__voter_id = id
         self.__party = party
         self.__opt_in = opt_in
+
+    def get_voter_id(self) -> str:
+        """
+        gets the voter's id number
+        :return: the voter's id number as a string
+        """
+        return self.__voter_id
+
+    def get_party(self) -> str:
+        """
+        gets the voter's party
+        :return: the voter's party as a string
+        """
+        return self.__party
+
+    def get_opt_in(self) -> str:
+        """
+        gets the voter's opt_in choice
+        :return: string "Yes" or "No"
+        """
+        return self.__opt_in
